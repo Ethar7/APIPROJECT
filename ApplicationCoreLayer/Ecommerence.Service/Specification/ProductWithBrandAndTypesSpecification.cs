a06da13e7f0f4769ac41a24f694ed3515a524457
Ethar7 <nabihethar@yahoo.com>
2025-12-06 05:47:26 +0200

    Enhancement Product

--- ApplicationCoreLayer/Ecommerence.Service/Specification/ProductWithBrandAndTypesSpecification.cs
+++ ApplicationCoreLayer/Ecommerence.Service/Specification/ProductWithBrandAndTypesSpecification.cs
@@ -6,9 +6,7 @@ namespace Ecommerence.Service.Specification
 {
     public class ProductWithBrandAndTypesSpecification : BaseSpecification<Product , int>
     {
-        public ProductWithBrandAndTypesSpecification(ProductQueryParams queryParams) : base(P => (!queryParams.BrandId.HasValue || P.BrandId == queryParams.BrandId.Value) 
-        && (!queryParams.TypeId.HasValue || P.TypeId == queryParams.TypeId.Value) &&
-        (string.IsNullOrEmpty(queryParams.Search) || P.Name.ToLower().Contains(queryParams.Search.ToLower())))
+        public ProductWithBrandAndTypesSpecification(ProductQueryParams queryParams) : base(ProductSpecificationHelper.GetProductCriteria(queryParams))
         {
             AddInclude(P => P.ProductBrand);
             AddInclude(P => P.ProductType);
