e3a4d7bafb95a665a210a3622c8802b61042abdf
Ethar7 <nabihethar@yahoo.com>
2025-12-06 05:47:26 +0200

    Enhancement Product

--- ApplicationCoreLayer/Ecommerence.Service/Specification/ProductCountSpecification.cs
+++ ApplicationCoreLayer/Ecommerence.Service/Specification/ProductCountSpecification.cs
@@ -5,9 +5,7 @@ namespace Ecommerence.Service.Specification
 {
     public class ProductCountSpecification : BaseSpecification<Product , int>
     {
-        public ProductCountSpecification(ProductQueryParams queryParams) : base(P => (!queryParams.BrandId.HasValue || P.BrandId == queryParams.BrandId.Value) 
-        && (!queryParams.TypeId.HasValue || P.TypeId == queryParams.TypeId.Value) &&
-        (string.IsNullOrEmpty(queryParams.Search) || P.Name.ToLower().Contains(queryParams.Search.ToLower())))
+        public ProductCountSpecification(ProductQueryParams queryParams) : base(ProductSpecificationHelper.GetProductCriteria(queryParams))
         {
             
         }
